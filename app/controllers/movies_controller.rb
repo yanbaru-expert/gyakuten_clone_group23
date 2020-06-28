f89cd4ed9762b1126ab29beb7c9907d685910aba
Takuma Tajikawa <takumatajikawa@TakumanoMacBook-Pro.local>
2020-06-29 04:23:32 +0900

    コンローラの中身の一部削除

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,26 +3,4 @@ class MoviesController < ApplicationController
     @movies = Movie.all
   end
 
-  def new
-
-  end
-
-  def create
-    flash[:notice] = "投稿しました"
-  end
-
-  def show
-
-  end
-
-  def edit
-  end
-
-  def update
-    flash[:notice] = "更新しました"
-  end
-
-  def destroy
-    flash.now[:alert] = "削除しました"
-  end
 end
