dcb72becdc67234d70df1ac85b65d7a155d6bd40
Takuma Tajikawa <takumatajikawa@TakumanoMacBook-Pro.local>
2020-06-28 14:40:37 +0900

    コントローラでフラッシュメッセージを設定

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,27 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def new
+
+  end
+
+  def create
+    flash[:notice] = "投稿しました"
+  end
+
+  def show
+
+  end
+
+  def edit
+  end
+
+  def update
+    flash[:notice] = "更新しました"
+  end
+
+  def destroy
+    flash.now[:alert] = "削除しました"
+  end
 end
