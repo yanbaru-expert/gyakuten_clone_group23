5cae9f687c4109c1637e7d269392ea99ced870f8
Takuma Tajikawa <takumatajikawa@TakumanoMacBook-Pro.local>
2020-06-30 06:36:04 +0900

    environment.jsの追加

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,7 @@
 const { environment } = require('@rails/webpacker')
 
+environment.plugins.append('Provide', new webpack.ProvidePlugin({	
+  $: 'jquery',	
+  jQuery: 'jquery'	
+}))
 module.exports = environment
