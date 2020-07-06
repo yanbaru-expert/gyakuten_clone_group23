715c9705b5c181cf9b40e3b396e00fb51922d50d
Shunsuke Noguchi <bakutensanba89@yahoo.ne.jp>
2020-07-06 14:41:37 +0900

    質問集の質問投稿ページを装（途中）

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,14 @@
 class QuestionsController < ApplicationController
   def index
     @questions = Question.order(id: :DESC) 
+    @question = Question.new
+  end
+  def create
+    Question.create(question_params)
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:title, :detail)
   end
 end
