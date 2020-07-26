ab842027c7ea26c18a8dbf38e0be935037a25d46
Takuma Tajikawa <takumatajikawa@TakumanoMacBook-Pro.local>
2020-07-26 14:10:54 +0900

    モデルの設定

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,8 @@
 class Movie < ApplicationRecord
   validates :title, :url, presence: true
+  has_many :watched_buttons
+
+  def watched_by?(user)
+    watched_buttons.where(user_email: user.email).exists?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :watched_buttons
 end
