760655635011d5cab99b3ff0e92e41648f02be5f
Takuma Tajikawa <takumatajikawa@TakumanoMacBook-Pro.local>
2020-07-26 16:51:01 +0900

    モデルとビューの変更

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,6 @@ class Movie < ApplicationRecord
   has_many :watched_buttons
 
   def watched_by?(current_user)
-    watched_buttons.where(user_email: current_user.email).exists?
+    watched_buttons.where(user_id: current_user.id).exists?
   end
 end
