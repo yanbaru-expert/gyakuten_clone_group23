5c7cca46794c1bf114778a888d1714795ca75426
Takuma Tajikawa <takumatajikawa@TakumanoMacBook-Pro.local>
2020-07-26 14:40:50 +0900

    コントローラの設定

--- app/controllers/watched_buttons_controller.rb
+++ app/controllers/watched_buttons_controller.rb
@@ -1,7 +1,11 @@
 class WatchedButtonsController < ApplicationController
   def create
+    watched_button = current_user.watched_buttons.build(movie_title: params[:movie_title])
+    watched_button.save
   end
 
   def destroy
+    watched_button = Watched_button.find(movie_title: params[:movie_title], user_email: current_user.email)
+    watched_button.destroy
   end
 end
